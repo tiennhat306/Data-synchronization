7d9c13b992813e0dff28c6acac05082b3ae73c43
Nhat <102210119@sv1.dut.udn.vn>
2023-10-24 16:37:51 +0700

    update MainApp

--- src/main/java/applications/MainApp.java
+++ src/main/java/applications/MainApp.java
@@ -6,12 +6,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/view/hello/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/view/client/dashboard.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 960, 540);
+//        scene.getStylesheets().add(Objects.requireNonNull(MainApp.class.getResource("/assets/css/style_hmm.css")).toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
