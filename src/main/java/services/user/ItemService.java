c4609239dfd6d3b379c461b619b9a72444cee01c
Nhat <102210119@sv1.dut.udn.vn>
2023-10-24 16:36:37 +0700

    update (client): create ItemService

--- src/main/java/services/user/ItemService.java
+++ src/main/java/services/user/ItemService.java
@@ -1,8 +1,13 @@
 package services.user;
 
-import models.Item;
+import javafx.util.Pair;
+import models.File;
+import models.Folder;
+import DTO.Item;
 import org.hibernate.Session;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ItemService {
@@ -14,15 +19,73 @@ public class ItemService {
         this.session = session;
     }
 
-    public List<Item> getAllItem() {
+    public List<Item> getAllItem(int folderId){
         try {
-            List<Item> query = session.createQuery("from Item", Item.class).list();
-            return query;
+            List<Item> itemList = new ArrayList<>();
+            List<Folder> folderList = session.createQuery("select fd from Folder fd where fd.parentId = :folderId", Folder.class)
+                    .setParameter("folderId", folderId)
+                    .list();
+            System.out.println("folderList: " + folderList);
+            if(folderList != null) {
+                for (Folder folder : folderList) {
+                    Item folderItem = new Item();
+                    folderItem.setId(folder.getId());
+                    folderItem.setTypeId(0);
+                    folderItem.setName(folder.getFolderName());
+                    folderItem.setOwnerName(folder.getUsersByOwnerId().getName());
+
+                    FolderService folderService = new FolderService(session);
+                    Pair<Date, Integer> updatedFolderInfo = folderService.getLastModifiedInfo(folder.getId());
+                    folderItem.setDateModified(updatedFolderInfo.getKey());
+
+                    UserService userService = new UserService(session);
+                    folderItem.setLastModifiedBy(updatedFolderInfo.getValue() == null ? "" : userService.getUserById(updatedFolderInfo.getValue()).getName());
+
+
+                    int countItem = folderService.getNumberItemOfFolder(folder.getId());
+                    folderItem.setSize(countItem + " mục");
+
+                    System.out.println("folderItem: " + folderItem);
+                    itemList.add(folderItem);
+                }
+            }
+
+            List<File> fileList = session.createQuery("select f from File f where f.folderId = :folderId", File.class)
+                    .setParameter("folderId", folderId)
+                    .list();
+            System.out.println("fileList: " + fileList);
+            if(fileList != null) {
+                for (File file : fileList) {
+                    Item fileItem = new Item();
+                    fileItem.setId(file.getId());
+                    fileItem.setTypeId(file.getTypeId());
+                    fileItem.setName(file.getName() + "." + file.getTypesByTypeId().getName());
+                    fileItem.setOwnerName(file.getUsersByOwnerId().getName());
+                    fileItem.setDateModified(file.getUpdatedAt());
+                    fileItem.setLastModifiedBy(file.getUpdatedBy() == null ? "" : file.getUsersByUpdatedBy().getName());
+
+                    String sizeString = "";
+                    long size = file.getSize();
+                    if (size < 1024) {
+                        sizeString = size + " B";
+                    } else if (size < 1024 * 1024) {
+                        sizeString = size / 1024 + " KB";
+                    } else if (size < 1024 * 1024 * 1024) {
+                        sizeString = size / 1024 / 1024 + " MB";
+                    } else {
+                        sizeString = size / 1024 / 1024 / 1024 + " GB";
+                    }
+                    fileItem.setSize(sizeString);
+                    System.out.println("fileItem: " + fileItem);
+                    itemList.add(fileItem);
+                }
+            }
+
+            return itemList;
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
-
 
 }
